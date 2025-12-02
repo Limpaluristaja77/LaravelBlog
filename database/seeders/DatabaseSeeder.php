1cfa705660d6b37753bb893ecbf295b9a311c8e2
Andri Reinek <andri.reinek@tptlive.ee>
2025-12-02 15:08:29 +0200

    Add likes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PostSeeder::class);
         $this->call(CommentSeeder::class);
         $this->call(TagSeeder::class);
+        $this->call(LikeSeeder::class);
     }
 }
