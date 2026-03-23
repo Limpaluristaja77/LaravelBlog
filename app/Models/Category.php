8aa11daec868b30e8b9fe6144edad073ba4e2440
Andri Reinek <andri.reinek@tptlive.ee>
2026-03-23 09:44:52 +0200

    Fix Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,66 +1,24 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use App\Models\Category;
-use App\Http\Requests\StoreCategoryRequest;
-use App\Http\Requests\UpdateCategoryRequest;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class CategoryController extends Controller
+class Category extends Model
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
+
+    public function children() {
+        return $this->hasMany(Category::class);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function parent() {
+        return $this->belongsTo(Category::class);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreCategoryRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateCategoryRequest $request, Category $category)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Category $category)
-    {
-        //
+    public function posts() {
+        return $this->hasMany(Post::class);
     }
 }
